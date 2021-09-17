36f7547b7267712ed9a476e9fab62b310e431a6b
riksanagara <58680975+riksanagara@users.noreply.github.com>
2021-09-17 09:05:13 +0700

    menyelesaikan video pembelajaran oop dasar pada PHP #2 - Class dan Object

---
+++ coba.php
@@ -0,0 +1,22 @@
+<?php
+/** 
+ * Riksa Kusumah Nagara
+ * 193040078
+ * https://github.com/riksanagara/prakweb2021_oophp_193040078
+ * Pertemuan 17 September 2021
+ * Mempelajari cara sederhana untuk membuat class dan instansiasi object pada php
+*/
+
+class Coba {
+
+
+
+}
+
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
---
+++ produk.php
@@ -0,0 +1,13 @@
+<?php
+
+// Jualan Produk
+// Komik
+// Game
+
+Class Produk {
+
+
+}
+
+$produk1 = new Produk();
+$produk1 = new Produk();
